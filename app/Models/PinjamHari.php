1ba32fffaf06c45cc97aec8742e4921050837cf1
Evan-aja <gebel909@student.ub.ac.id>
2022-04-29 15:52:22 +0700

    controller buat daftar

--- app/Models/PinjamHari.php
+++ app/Models/PinjamHari.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PinjamBulan extends Model
+class PinjamHari extends Model
 {
     public function peminjam(){
         return $this->belongsTo('App\Models\Peminjam');
--- resources/views/createPinjamHari.blade.php
+++ resources/views/createPinjamHari.blade.php
@@ -8,7 +8,7 @@
     <br/>
     <div class="panel panel-primary">
         <div class="panel-heading">
-            Add Pinjam Bulan Details
+            Add Pinjam Hari Details
         </div>
         <div class="panel-body">
             <form method="post" action="{{ route('pinjambulan.store') }}">
