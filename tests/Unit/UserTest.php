545a1bf8c90c91d4eaa6c649c348c9186fff8dc4
miftahulihsan <vjmiftahulihsan@gmail.com>
2022-05-24 22:20:07 +0700

    buat UserTest.php untuk testing

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $a = 2+1;
+        $this->assertTrue($a == 3);
+    }
+}
